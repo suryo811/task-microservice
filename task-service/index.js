dcb981101e880d193798bd3043ce2c77b52a1c47
suryoday <suryodaykaushik@gmail.com>
2025-11-16 12:07:38 +0530

    Enhance Docker configuration and add notification service with RabbitMQ integration. Updated docker-compose.yml to include RabbitMQ service and conditions for service dependencies. Introduced notification-service with package.json and package-lock.json, and implemented RabbitMQ connection in task-service for task message handling.

--- task-service/index.js
+++ task-service/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import amqp from "amqplib";
 
 dotenv.config();
 
@@ -25,11 +26,39 @@ const taskSchema = new mongoose.Schema({
 
 const Task = mongoose.model("Task", taskSchema);
 
+let connection,channel;
+
+// rabbitmq connection
+async function connectRabbitMQWithRetry(retries = 3, delay = 3000){
+    console.log(`Attempting to connect to RabbitMQ (${retries} retries remaining)...`);
+    while(retries){
+        try {
+            connection = await amqp.connect("amqp://rabbitmq:5672")
+            channel = await connection.createChannel()
+            await channel.assertQueue("task_queue", {durable: true})
+            return true;
+        } catch (error) {
+            console.log(`Failed to connect to RabbitMQ, retries left: ${retries}`, error.message)
+            await new Promise(resolve => setTimeout(resolve, delay))
+            retries--
+        }
+    }
+    throw new Error("Failed to connect to RabbitMQ")
+}
+
 // route to create a new task
 app.post("/tasks", async (req,res) => {
     const {title, description, userId} = req.body;
     try {
         const task = await Task.create({title, description});
+        const message = {
+            taskId: task._id,
+            title: task.title,
+            description: task.description,
+            userId: task.userId
+        }
+        await channel.sendToQueue("task_queue", Buffer.from(JSON.stringify(message)))
+        console.log(`Task created and sent to RabbitMQ: ${task._id}`)
         res.status(201).json({message: "Task created successfully", task : {id: task._id, title: task.title, description: task.description, userId: task.userId}});
     } catch (error) {
         res.status(500).json({message: "Internal server error"});
@@ -50,4 +79,10 @@ const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
     console.log(`Task Service is running on port ${PORT} 🚀`);
+    console.log("Starting RabbitMQ connection...");
+    connectRabbitMQWithRetry().then(() => {
+        console.log("RabbitMQ connected successfully")
+    }).catch((err) => {
+        console.error("Failed to connect to RabbitMQ after all retries:", err.message)
+    })
 });
